ca28a212e7d3fd3da72f2f6f23af89bec2b69650
JLaurence4554 <salomonjohnlaurence391@gmail.com>
2025-01-23 21:44:52 +0800

    first commit
    1-23-2025, 9:44

---
+++ WebApplication1.API/WebApplication1.API/Models/RentalDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.API.Models
+{
+    public class RentalDbContext : DbContext
+    {
+        public RentalDbContext(DbContextOptions<RentalDbContext> options) : base(options) { }
+        public DbSet<costumer> costumers { get; set; }
+        public DbSet<movie> movies { get; set; }
+        public DbSet<RentalMovie> rentals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<costumer>().HasKey(c => c.costumerID);
+            modelBuilder.Entity<movie>().HasKey(m => m.movieID);
+            modelBuilder.Entity<RentalMovie>().HasKey(r => r.RentalDuration);
+
+            modelBuilder.Entity<RentalMovie>()
+                .HasOne(e => e.costumerID)
+                .WithMany(m => m.movieID)
+                .HasForiegnKey( r => r.RentalMovie)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<RentalMovie>()
+                .HasOne(e => e.costumerID)
+                .WithMany(m => m.movieID)
+                .HasForiegnKey(r => r.RentalMovie)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+    }
+}
---
+++ WebApplication1.API/WebApplication1.API/Models/UpdateMovie.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.API.Models
+{
+    public class UpdateMovie
+    {
+        public int movieID { get; set; }
+        public required string title { get; set; }
+        public string movieprize { get; set; }
+    }
+}
